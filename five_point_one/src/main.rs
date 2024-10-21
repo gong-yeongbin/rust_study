4b76367efa159b29f88400fec2f9d13d1f3072a6
gong-yeongbin <youngbin816@gmail.com>
2024-10-22 06:44:33 +0900

    구조체 정의 및 인스턴트화

---
+++ five_point_one/src/main.rs
@@ -0,0 +1,56 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+struct Color(i32, i32, i32);
+
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("anothre@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    let user2 = User {
+        email: String::from("anothre@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
