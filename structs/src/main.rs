a04b09f26e8fcc9a8ec4edecdc285fb96171a33d
gong-yeongbin <youngbin816@gmail.com>
2024-11-12 07:02:49 +0900

    구초체 정의

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,6 +6,12 @@ struct User {
     sign_in_count: u64,
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     // [5.1] 구조체 정의 및 인스턴스화
     /* 일부 필드만 가변으로 만들 수 없음 */
@@ -25,6 +31,47 @@ fn main() {
         email: String::from("another@example.com"),
         ..user1
     };
+
+    // [5.1.3] 명명된 필드 없는 튜플 구조체를 사용하여 다른 타입 만들기
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // [5.1.4] 필드가 없는 유사 유닛 구조체
+    struct AlwaysEqual;
+
+    let subject = AlwaysEqual;
+
+    // [5.2] 구조체를 사용한 예제 프로그램
+    // let width1 = 30;
+    // let height1 = 50;
+    //
+    // println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+
+    // [5.2.1] 튜플로 리팩터링하기
+    // let rect1 = (30, 50);
+    // println!("The area of the rectangle is {} square pixels.", area(rect1));
+
+    // [5.2.2] 구조체로 리팩터링하여 코드에 더 많은 의미를 담기
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+
+    // [5.2.3] 트레이트 파생으로 유용한 기능 추가하기
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("rect1 is {:#?}", rect1);
+
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
 }
 
 // [5.1.1] 필드 초기화 축약법 사용하기
@@ -35,4 +82,16 @@ fn build_user(email: String, username: String) -> User {
         email,
         sign_in_count: 1,
     }
+}
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+fn area(recttangle: &Rectangle) -> u32 {
+    recttangle.width * recttangle.height
 }
