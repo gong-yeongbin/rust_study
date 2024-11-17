6c4c9a1364752cc39b1703089c3af59594c39cd4
gong-yeongbin <youngbin816@gmail.com>
2024-11-17 15:35:10 +0900

    파일로 모듈 분리

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,65 +1,88 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//
+//         fn seat_at_table() {}
+//     }
+//
+//     mod serving {
+//         fn take_order() {}
+//
+//         fn serve_order() {}
+//
+//         fn take_payment() {}
+//     }
+// }
+//
+// fn deliver_order() {}
+//
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::deliver_order();
+//     }
+//
+//     fn cook_order() {}
+//
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+//
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             crate::back_of_house::Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+//
+//     pub enum Appetizer {
+//         Soup,
+//         Salad,
+//     }
+// }
+//
+// use crate::front_of_house::hosting::add_to_waitlist;
 
-        fn seat_at_table() {}
-    }
+// pub fn eat_at_restaurant() {
+//     // 절대 경로
+//     // crate::front_of_house::hosting::add_to_waitlist();
+//     // 상대 경로
+//     // front_of_house::hosting::add_to_waitlist();
+//
+//     // 호밀(Rye) 토스트를 곁들인 여름철 조식 주문하기
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     // 먹고 싶은 빵 바꾸기
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+//
+//
+//     // 컴파일 에러
+//     // meal.seasonal_fruit = String::from("blueberrires")
+//
+//     let order1 = back_of_house::Appetizer::Soup;
+//     let order2 = back_of_house::Appetizer::Salad;
+// }
 
-    mod serving {
-        fn take_order() {}
+// pub fn eat_at_restaurant() {
+//     add_to_waitlist();
+// }
 
-        fn serve_order() {}
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-        fn take_payment() {}
-    }
-}
+// fn function1() -> Result {}
+//
+// fn function2() -> IoResult<()> {}
 
-fn deliver_order() {}
+// pub use crate::front_of_house::hosting;
 
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
+mod front_of_house;
 
-    fn cook_order() {}
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            crate::back_of_house::Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    // 절대 경로
-    crate::front_of_house::hosting::add_to_waitlist();
-    // 상대 경로
-    front_of_house::hosting::add_to_waitlist();
-
-    // 호밀(Rye) 토스트를 곁들인 여름철 조식 주문하기
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // 먹고 싶은 빵 바꾸기
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
-
-
-    // 컴파일 에러
-    // meal.seasonal_fruit = String::from("blueberrires")
-
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    hosting::add_to_waitlist();
 }
---
+++ restaurant/src/main.rs
@@ -0,0 +1,16 @@
+use rand::Rng;
+use std::collections::HashMap;
+
+// use std::cmp::Ordering;
+// use std::io;
+// use std::{cmp::Ordering, io};
+
+// use std::io;
+// use std::io::Write;
+use std::io::{self, Write};
+
+use std::collections::*;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+}
