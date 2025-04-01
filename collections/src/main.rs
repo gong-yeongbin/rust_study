650876febcd9aea1cb5a1eff5b0278c9e8144b67
gong-yeongbin <youngbin816@gmail.com>
2025-04-01 15:32:44 +0900

    문자열

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 fn main() {
     let v: Vec<i32> = Vec::new();
     let v = vec![1, 2, 3];
@@ -56,4 +58,41 @@ fn main() {
     {
         let v = vec![1, 2, 3, 4];
     }
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+
+    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("s is {s}");
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    println!("{s3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    // let s = s1 + "-" + &s2 + "-" + &s3;
+    // println!("{s}");
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{s}");
+
+    let s1 = String::from("hello");
+    let h = s1[0];
 }
