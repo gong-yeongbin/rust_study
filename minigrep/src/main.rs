4ab20ea33b50de06010023f8af563f851248f801
gong-yeongbin <youngbin816@gmail.com>
2025-04-08 14:53:04 +0900

    minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,15 @@
+use minigrep::{Config, run};
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        process::exit(1);
+    }
+}
