c312cf987f7031966a91fc0068cfeb079e8cc0fb
gong-yeongbin <youngbin816@gmail.com>
2025-05-07 11:02:22 +0900

    Rc<T>

--- smartpointers/src/main.rs
+++ smartpointers/src/main.rs
@@ -47,19 +47,6 @@ fn main() {
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
 
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    let c = CustomSmartPointer {
-        data: String::from("some data"),
-    };
-    println!("CustomSmartPointers created.");
-    drop(c);
-    println!("CustomSmartPointer dropped before the end of main.");
-
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
@@ -68,7 +55,10 @@ fn main() {
         let c = Cons(4, Rc::clone(&a));
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a))
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    let mut x = 5;
+    let y = &mut x;
 }
 fn hello(name: &str) {
     println!("Hello, {name}!");
